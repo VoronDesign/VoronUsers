c2c8fad40304da9da236dcd207dcd7a2197cf2c4
Benoit Miller <benoit.miller@gmail.com>
2020-01-27 19:40:28 -0500

    Fix typo in path, add note to backup batch file

--- firmware_configurations/reprapfirmware/fulg/update.bat
+++ firmware_configurations/reprapfirmware/fulg/update.bat
@@ -1,3 +1,6 @@
+@rem Update the URL below to match the name you've set at M550 in sys/config.g!
+@rem The .local suffix is automatically added to that name.
+
 wget --ftp-user=admin -r -nH ftp://voronv2015.local/macros
 wget --ftp-user=admin -r -nH ftp://voronv2015.local/sys
 
